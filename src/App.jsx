c3b179ead478431c4f497aa828b4ec7d47de8821
GeekyMonk07 <thelearninggeek07@gmail.com>
2023-11-18 13:35:54 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { getApiConfigurations, getGenres } from './store/homeSlice';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
-import PageNotFound from './pages/404/pageNotFound';
+import PageNotFound from './pages/404/PageNotFound';
 import Details from './pages/details/Details';
 import SearchResult from './pages/searchResult/SearchResult';
 import Explore from './pages/explore/Explore';
